6ed8247fe77d1268705adbc682fef8ea1551c46b
chris0512 <kyang3200@gmail.com>
2022-05-25 22:31:37 +0800

    add configuration and spring will scan the class MyDataRestConfig

--- src/main/java/com/chris/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/chris/ecommerce/config/MyDataRestConfig.java
@@ -2,12 +2,13 @@ package com.chris.ecommerce.config;
 
 import com.chris.ecommerce.entity.Product;
 import com.chris.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
